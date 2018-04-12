7f2178e7fe382d09b07adeb4ed9b207b7c4c8228
Yoonmee <ymhwang119@naver.com>
2018-04-12 23:10:35 +0900

    add multiplying.cpp

---
+++ multiplying.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int i = 0; i < 9; i++)
+	{
+		cout << i + 1 << "´Ü : " << endl;
+		for (int j = 0; j < 10; j++)
+		{
+			cout << i + 1 << "*" << j + 1 << " = " << (i + 1)*(j + 1) << endl;
+		}
+		cout << endl;
+	}
+}
